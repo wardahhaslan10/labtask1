0ee2ea553333260442a6c576f60d417716eccb57
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-08-13 15:49:43 +0800

    push

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ include 'app.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>String Array analysis</title>
     <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        rel="stylesheet">
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet">
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="styles.css">
